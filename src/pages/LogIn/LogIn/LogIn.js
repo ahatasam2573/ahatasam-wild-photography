6d1fb0b607a077d9885e2705606f6791235ed59d
ahatasam2573 <ahatasamul15-3503@diu.edu.bd>
2022-04-19 02:41:39 +0600

    LogIn added

---
+++ src/pages/LogIn/LogIn/LogIn.js
@@ -0,0 +1,92 @@
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import Loading from '../../Shared/Loading/Loading';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const Login = () => {
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const location = useLocation();
+    let errorElement;
+
+    let from = location.state?.from?.pathname || "/";
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+
+        signInWithEmailAndPassword(email, password);
+    }
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+    const resetPassword = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Sent email');
+        }
+        else {
+            toast('Please Enter Your Email Address');
+        }
+    }
+
+    if (loading || sending) {
+        return <Loading></Loading>
+    }
+
+    if (user) {
+        navigate(from, { replace: true });
+    }
+
+    if (error) {
+
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+
+    const navigateRegister = event => {
+        navigate('/register');
+    }
+    return (
+        <div className='container w-50 mx-auto'>
+            <h2 className='text-primary text-center mt-2'>Please Login</h2>
+            <Form onSubmit={handleLogin}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                <Button variant="primary w-50 mx-auto d-block" type="submit">
+                    Login
+                </Button>
+            </Form>
+            {errorElement}
+            <p className='mt-2'>New to Siam Wild Photography? <span style={{ cursor: 'pointer' }}
+                className='text-primary' onClick={navigateRegister} >Please Register</span></p>
+            <p className='mt-2'>Forget Password? <span style={{ cursor: 'pointer' }}
+                className='text-primary' onClick={resetPassword} >Reset Your Password</span></p>
+            <SocialLogin></SocialLogin>
+            <ToastContainer />
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/LogIn/Register/Register.js
@@ -0,0 +1,80 @@
+import React, { useRef, useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import SocialLogin from '../SocialLogin/SocialLogin';
+import Loading from '../../Shared/Loading/Loading';
+
+const Register = () => {
+    const [agree, setAgree] = useState(false);
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+
+    const nameRef = useRef('');
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+
+    if (user) {
+        navigate('/home');
+    }
+
+    if (loading) {
+        return <Loading></Loading>
+    }
+
+    const handleRegister = event => {
+        event.preventDefault();
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        if (agree) {
+            createUserWithEmailAndPassword(email, password);
+        }
+
+
+    }
+    const navigateRegister = event => {
+        navigate('/login');
+    }
+
+    return (
+        <div className='container w-50 mx-auto'>
+            <h2 className='text-primary text-center mt-2'>Please Resister</h2>
+            <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your Name</Form.Label>
+                    <Form.Control ref={nameRef} type="text" placeholder="Enter Your Name" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                <input className='me-2' onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={agree ? 'text-primary' : 'text-danger'} htmlFor="terms">Accept Terms and Conditions</label>
+                <Button
+                    disabled={!agree}
+                    variant="primary w-50 mx-auto d-block mt-2" type="submit">
+                    Register
+                </Button>
+            </Form>
+            <p className='mt-2'>Already have an Account? <span style={{ cursor: 'pointer' }}
+                className='text-primary' onClick={navigateRegister} >Please Login</span></p>
+            <SocialLogin></SocialLogin>
+        </div>
+    );
+};
+
+export default Register;
